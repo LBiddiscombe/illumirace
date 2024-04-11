bfed233fc313969421249bdbbfb7db51141c5210
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2024-04-11 11:06:25 +0100

    update bg url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="mx-auto grid h-screen max-h-svh place-items-center bg-[linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.8)),url('images/bg-wall.jpeg')] bg-cover pt-4 md:container md:max-h-[768px] md:max-w-md"
+	class="mx-auto grid h-screen max-h-svh place-items-center bg-[linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.8)),url('/images/bg-wall.jpeg')] bg-cover pt-4 md:container md:max-h-[768px] md:max-w-md"
 >
 	<slot />
 </div>
